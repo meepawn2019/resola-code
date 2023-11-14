1c84e1c3adb09fa8a6795e14da3aabf63f824ec0
meepawn2019 <meepawn2018@gmail.com>
2023-11-14 18:49:03 +0700

    Check log fail for CI/CD task

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   if (err) {
+    console.log(err);
     res.status(err.status || 500).json({
       message: err.message || 'Internal Server Error',
     });
